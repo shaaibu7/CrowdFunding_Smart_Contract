a6950002528b210257f7147b8e190e90529ac3c7
shaaibu suleiman <shaaibusuleiman9@gmail.com>
2024-08-24 18:31:00 +0100

    complete smart contract v1

---
+++ contracts/CrowdFunding.sol
@@ -0,0 +1,91 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract crowdFunding {
+    address owner;
+
+    // this mapping handles the status of donation
+    mapping(string => bool) endDonation;
+
+    // using constant to avoid magic numbers
+    uint constant private ZERO = 0;
+
+    // This modifier handles calling the adminWithdraw  function to only the admin
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    constructor() {
+        // Give ownership of the contract to the deployer of the contract
+        owner = msg.sender;
+    }
+
+    // Creating Events
+    event CampaignCreated(uint campaignID);
+    event DonationReceived(uint campaignID);
+    event CampaignEnded(uint campaignID);
+
+    // structure of creating a new campaign and the corresponding values it should have
+    struct Campaign {
+        string title;
+        string description;
+        address benefactor;
+        uint goal;
+        uint duration;
+        uint deadline;
+        uint amountRaised;
+    }
+
+    // storing all created campaigns in an array to keep track of them
+    Campaign[] public campaigns;
+
+    function createCampaign(string memory _title, string memory  _description, address _benefactor, uint _goal, uint duration) public {
+        Campaign storage campaign = campaigns.push();
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.benefactor = _benefactor;
+        campaign.goal = _goal;
+        campaign.duration = duration;
+        campaign.deadline = block.timestamp + duration;
+        require(_goal > ZERO); // validating that the goal provided is greater than zero
+        emit CampaignCreated(campaigns.length + 1);
+    }
+
+    function donate(uint _campaignId) public payable  {
+        for(uint256 i = 0; i < campaigns.length; i++) {
+            require((campaigns[i].deadline) < block.timestamp);
+            require(!endDonation[campaigns[_campaignId].title]); // reject donation after end of campaign
+            if(campaigns[i].goal == campaigns[_campaignId].goal) {
+                 campaigns[i].amountRaised +=  msg.value;
+            }
+        }
+        emit DonationReceived(_campaignId);
+    }
+
+    function endCampaign() public {
+        for(uint256 i = 0; i < campaigns.length; i++) {
+            if(campaigns[i].deadline > block.timestamp) {
+                endDonation[campaigns[i].title] = true; // handle end of campaign
+                uint amount = campaigns[i].amountRaised;
+                (bool success,) = (campaigns[i].benefactor).call{ value: amount}("");
+                require(success);
+                emit DonationReceived(i);
+            }
+        }
+    
+    }
+
+    function adminWithdraw() public onlyOwner {
+        for(uint i = 0; i < campaigns.length; i++) {
+            if(endDonation[campaigns[i].title] && campaigns[i].amountRaised > 0) {
+                uint amount = campaigns[i].amountRaised;
+                (bool success, ) = (msg.sender).call{ value: amount }("");
+                require((success));
+
+            }
+        }
+    }
+
+
+}
---
+++ hardhat.config.js
@@ -0,0 +1,18 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${procces.env.ALCHEMYAPIKEY}`,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: [process.env.ETHERSCAN_API_KEY]
+    }
+  }
+};

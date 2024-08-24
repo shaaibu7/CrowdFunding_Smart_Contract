ac38b13e903f6d7b4a8c839ee6f86eea70a6e751
shaaibu suleiman <shaaibusuleiman9@gmail.com>
2024-08-24 18:35:04 +0100

    Update hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,7 +6,7 @@ module.exports = {
   solidity: "0.8.24",
   networks: {
     sepolia: {
-      url: `https://eth-sepolia.g.alchemy.com/v2/${procces.env.ALCHEMYAPIKEY}`,
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
       accounts: [process.env.PRIVATE_KEY]
     }
   },
